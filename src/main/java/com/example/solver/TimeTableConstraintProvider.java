cd48ce61071f2932f491cce5e5b46872bad82554
Crushedkrz78 <crispmolinito93@gmail.com>
2020-08-24 13:41:24 -0500

    [Spring-Boot] Created the Time Table Constraint Provider Class

---
+++ src/main/java/com/example/solver/TimeTableConstraintProvider.java
@@ -0,0 +1,77 @@
+package com.example.solver;
+
+import com.example.domain.Lesson;
+import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
+import org.optaplanner.core.api.score.stream.Constraint;
+import org.optaplanner.core.api.score.stream.ConstraintFactory;
+import org.optaplanner.core.api.score.stream.ConstraintProvider;
+import org.optaplanner.core.api.score.stream.Joiners;
+
+public class TimeTableConstraintProvider implements ConstraintProvider {
+    /**
+     * A 'score' represents the quality of a specific solution, the higher the better. OptaPlanner looks for
+     * the best solution, which is the solution with the highest score found in the available time.
+     * It might be the optimal solution.
+     *
+     * This use case has Hard and Soft Constraints, so the HardSoftScore class represents the score.
+     *
+     *  - Hard constraints must not be broken.
+     *  - Soft constraints should not be broken.
+     *
+     *  Hard constraints are weighted against other hard constraints. Soft constraints are weighted too, against
+     *  other soft constraints.
+     *  Hard Constraints alwais outweight Soft Constraints, regardless of their respective weights.
+     *  
+     * @param constraintFactory
+     * @return
+     */
+
+    @Override
+    public Constraint[] defineConstraints(ConstraintFactory constraintFactory) {
+        return new Constraint[] {
+                // Hard constraints
+                roomConflict(constraintFactory),
+                teacherConflict(constraintFactory),
+                studentGroupConflict(constraintFactory),
+                // Soft constraints are only implemented in the "complete" implementation
+        };
+    }
+
+    private Constraint roomConflict(ConstraintFactory constraintFactory) {
+        // A room can accommodate at most one lesson at the same time.
+
+        // Select a lesson ...
+        return constraintFactory.from(Lesson.class)
+                // ... and pair it with another lesson ...
+                .join(Lesson.class,
+                        // ... in the same timeslot ...
+                        Joiners.equal(Lesson::getTimeslot),
+                        // ... in the same room ...
+                        Joiners.equal(Lesson::getRoom),
+                        // ... and the pair is unique (different id, no reverse pairs)
+                        Joiners.lessThan(Lesson::getId))
+                // then penalize each pair with a hard weight.
+                .penalize("Room conflict", HardSoftScore.ONE_HARD);
+    }
+
+    private Constraint teacherConflict(ConstraintFactory constraintFactory) {
+        // A teacher can teach at most one lesson at the same time.
+        return constraintFactory.from(Lesson.class)
+                .join(Lesson.class,
+                        Joiners.equal(Lesson::getTimeslot),
+                        Joiners.equal(Lesson::getTeacher),
+                        Joiners.lessThan(Lesson::getId))
+                .penalize("Teacher conflict", HardSoftScore.ONE_HARD);
+    }
+
+    private Constraint studentGroupConflict(ConstraintFactory constraintFactory) {
+        // A student can attend at most one lesson at the same time.
+        return constraintFactory.from(Lesson.class)
+                .join(Lesson.class,
+                        Joiners.equal(Lesson::getTimeslot),
+                        Joiners.equal(Lesson::getStudentGroup),
+                        Joiners.lessThan(Lesson::getId))
+                .penalize("Student group conflict", HardSoftScore.ONE_HARD);
+    }
+
+}
